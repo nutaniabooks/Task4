71eaf4df512154f3ed97f9fcf96c04e8f614578f
Tania Gonzales <taniagonzales@Tanias-MacBook-Pro.local>
2023-12-03 20:20:21 -0500

    continue improvement

---
+++ app.js
@@ -0,0 +1,9 @@
+// Get a reference to the image element
+var jumpingImage1 = document.getElementById('jumpingImage1');
+
+// Add a click event listener to the image
+jumpingImage1.addEventListener('click', function() {
+    // Toggle the 'pop' class on and off
+    jumpingImage1.classList.toggle('pop');
+});
+
